52b86c4a738422341690ea95959edb0a8bf3e1eb
Frederic Deleze <frederic.deleze@gmail.com>
2013-07-11 15:03:37 +0000

    put code to deploy web.js on webpage

--- web.js
+++ web.js
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
-
+var express = require('express');
 var fs = require('fs');
 var infile = "index.html";
 var buffer = Buffer(50);
 buffer = fs.readFileSync(infile);
-console.log(buffer.toString());
+var outfile = buffer.toString();
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send(outfile);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
